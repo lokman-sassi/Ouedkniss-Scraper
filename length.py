bd0db588c5af63edc4153d9ad10a1576464a4fb1
SASSI Lokman <lokman.sassi@univ-constantine2.dz>
2024-04-24 14:41:25 +0100

    Updated repository

--- length.py
+++ length.py
@@ -1,14 +1,5 @@
-import json
 import pymongo
 
-url =  'https://api.ouedkniss.com/graphql'
-
-# Load the data from the JSON file
-with open('ouedkniss_immobilier.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
-            
-# Get the length of the scraped data
-print("Scraped items:", len(data))
 
 client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
 mydb = client["Real-Estate"]
